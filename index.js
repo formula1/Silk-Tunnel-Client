302ec4cddc4531e7cfc7e5e06c28d65b1011646d
formula1 <samtobia@gmail.com>
2014-11-30 00:25:00 -0800

    both frameworks

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var Tunnel2Proxy = require(__dirname+"/Tunnel2Proxy_com.js");
 
 var tunnel = new Tunnel2Proxy();
 
-methods.add("auth",function(data,call_ob,next){
-  tunnel.setServer(data.url,data.pass,next);
-})
+if(typeof ForkRouter == "undefined"){
+  methods.add({
+    "tunnelclient-auth":function(data,call_ob,next){
+      tunnel.setServer(data.url,data.pass,next);
+    }
+  })
+}else{
+  methods.add("auth",function(data,call_ob,next){
+    tunnel.setServer(data.url,data.pass,next);
+  })
+}
